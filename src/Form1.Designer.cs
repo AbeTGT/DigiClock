fe5b5f7ec756e6e096300f953dbf2d2c720afbcc
Abe <69663458+AbeTGT@users.noreply.github.com>
2022-12-30 20:53:24 +1300

    Add files via upload

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace DigiClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SettingLabel = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.setTextColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.themesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.WelcomeMsg = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(-3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 28);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(38, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DigiClock v0.02.0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label3_MouseDown);
+            this.label3.MouseEnter += new System.EventHandler(this.label3_MouseEnter);
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label3_MouseMove);
+            this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label3_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(267, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // SettingLabel
+            // 
+            this.SettingLabel.AutoSize = true;
+            this.SettingLabel.BackColor = System.Drawing.Color.Black;
+            this.SettingLabel.Font = new System.Drawing.Font("Malgun Gothic", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SettingLabel.ForeColor = System.Drawing.Color.White;
+            this.SettingLabel.Location = new System.Drawing.Point(180, 116);
+            this.SettingLabel.Name = "SettingLabel";
+            this.SettingLabel.Size = new System.Drawing.Size(111, 12);
+            this.SettingLabel.TabIndex = 4;
+            this.SettingLabel.Text = "Right click to view settings";
+            this.SettingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.setTextColorToolStripMenuItem,
+            this.themesToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(147, 70);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Checked = true;
+            this.toolStripMenuItem1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(146, 22);
+            this.toolStripMenuItem1.Text = "TopMost";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click_1);
+            // 
+            // setTextColorToolStripMenuItem
+            // 
+            this.setTextColorToolStripMenuItem.Name = "setTextColorToolStripMenuItem";
+            this.setTextColorToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.setTextColorToolStripMenuItem.Text = "Set Text Color";
+            this.setTextColorToolStripMenuItem.Click += new System.EventHandler(this.setTextColorToolStripMenuItem_Click);
+            // 
+            // themesToolStripMenuItem
+            // 
+            this.themesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.themesToolStripMenuItem.Name = "themesToolStripMenuItem";
+            this.themesToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.themesToolStripMenuItem.Text = "Themes";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "Blue City (Default)",
+            "Cyberpunk 2077"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // WelcomeMsg
+            // 
+            this.WelcomeMsg.BackColor = System.Drawing.Color.Black;
+            this.WelcomeMsg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.WelcomeMsg.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeMsg.ForeColor = System.Drawing.Color.White;
+            this.WelcomeMsg.Location = new System.Drawing.Point(0, 41);
+            this.WelcomeMsg.Name = "WelcomeMsg";
+            this.WelcomeMsg.Size = new System.Drawing.Size(291, 14);
+            this.WelcomeMsg.TabIndex = 5;
+            this.WelcomeMsg.Text = "Welcome, User.";
+            this.WelcomeMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = global::DigiClock.Properties.Resources.white_clock;
+            this.pictureBox2.Location = new System.Drawing.Point(7, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DigiClock.Properties.Resources.b4045820491130ade657939f75a30783;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(306, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 128);
+            this.Controls.Add(this.WelcomeMsg);
+            this.Controls.Add(this.SettingLabel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DigiClock - Widget";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label SettingLabel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem setTextColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem themesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.Label WelcomeMsg;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigiClock
+{
+    public partial class Form1 : Form
+    {
+        public string curVersion = "v0.01.0";
+        private bool mousedown;
+        private Point lastlocation;
+        private bool isContextMenuStripVisible = false;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = DateTime.Now.ToLongTimeString();
+            label2.Text = DateTime.Now.ToLongDateString();
+            this.TopMost = true;
+            var pos1 = this.PointToScreen(label1.Location);
+            pos1 = pictureBox1.PointToClient(pos1);
+            label1.Parent = pictureBox1;
+            label1.Location = pos1;
+            label1.BackColor = Color.Transparent;
+            var pos2 = this.PointToScreen(label2.Location);
+            pos2 = pictureBox1.PointToClient(pos2);
+            label2.Parent = pictureBox1;
+            label2.Location = pos2;
+            label2.BackColor = Color.Transparent;
+            label3.Parent = panel1;
+            var pos3 = this.PointToScreen(SettingLabel.Location);
+            pos3 = pictureBox1.PointToClient(pos3);
+            SettingLabel.Parent = pictureBox1;
+            SettingLabel.Location = pos3;
+            SettingLabel.BackColor = Color.Transparent;
+            var pos4 = this.PointToScreen(WelcomeMsg.Location);
+            pos4 = pictureBox1.PointToClient(pos4);
+            WelcomeMsg.Parent = pictureBox1;
+            WelcomeMsg.Location = pos4;
+            WelcomeMsg.BackColor = Color.Transparent;
+            WelcomeMsg.Text = "Welcome, " + Environment.UserName + ".";
+            toolStripMenuItem1.Checked = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+            label2.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousedown = true;
+            lastlocation = e.Location;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousedown)
+            {
+                this.Location = new Point((this.Location.X - lastlocation.X) + e.X, (this.Location.Y - lastlocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousedown = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.SetToolTip(button1, "Closes DigiClock.");
+        }
+
+        private void label3_MouseEnter(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.LimeGreen;
+            label3.Text = "DigiClock | 2022 AbeTGT";
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.White;
+            label3.Text = "DigiClock " + curVersion;
+        }
+
+        private void label3_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousedown = true;
+            lastlocation = e.Location;
+        }
+
+        private void label3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousedown)
+            {
+                this.Location = new Point((this.Location.X - lastlocation.X) + e.X, (this.Location.Y - lastlocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void label3_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousedown = false;
+        }
+
+        
+
+        private void toolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            if (toolStripMenuItem1.Checked == false)
+            {
+                this.TopMost = true;
+                toolStripMenuItem1.Checked = true;
+                MessageBox.Show("TopMost has successfully been set to true.\nDigiClock will now be on top of other applications.", "DigiClock | Settings");
+            } else
+            {
+                this.TopMost = false;
+                toolStripMenuItem1.Checked = false;
+                MessageBox.Show("TopMost has successfully been set to false.\nDigiClock will no longer be on top of other applications.", "DigiClock | Settings");
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (isContextMenuStripVisible == false)
+                {
+                    isContextMenuStripVisible = true;
+                } else if (isContextMenuStripVisible == true)
+                {
+                    isContextMenuStripVisible = false;
+                }
+                contextMenuStrip1.Visible = isContextMenuStripVisible;
+                contextMenuStrip1.Show(MousePosition.X, MousePosition.Y);
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (isContextMenuStripVisible == false)
+                {
+                    isContextMenuStripVisible = true;
+                }
+                else if (isContextMenuStripVisible == true)
+                {
+                    isContextMenuStripVisible = false;
+                }
+                contextMenuStrip1.Visible = isContextMenuStripVisible;
+                contextMenuStrip1.Show(MousePosition.X, MousePosition.Y);
+            }
+        }
+
+        private void setTextColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                label1.ForeColor = dlg.Color;
+                label2.ForeColor = dlg.Color;
+                SettingLabel.ForeColor = dlg.Color;
+                WelcomeMsg.ForeColor = dlg.Color;
+            }
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string toolString = toolStripComboBox1.SelectedItem.ToString();
+            if (toolString.Equals("Blue City (Default)"))
+            {
+                SetAllLabelsToColor(Color.White);
+                pictureBox1.Image = Properties.Resources.b4045820491130ade657939f75a30783;
+                MessageBox.Show("The default theme has been successfully applied.", "DigiClock | Themes");
+            } else if (toolString.Equals("Cyberpunk 2077"))
+            {
+                SetAllLabelsToColor(Color.FromArgb(255, 191, 244));
+                pictureBox1.Image = Properties.Resources.Cyberpunk2077;
+                MessageBox.Show("The Cyberpunk 2077 theme has been successfully applied.", "DigiClock | Themes");
+            }
+        }
+
+        void SetAllLabelsToColor(Color chosenColor)
+        {
+            label1.ForeColor = chosenColor;
+            label2.ForeColor = chosenColor;
+            SettingLabel.ForeColor = chosenColor;
+            WelcomeMsg.ForeColor = chosenColor;
+        }
+    }
+}
